d51f35e8d8e9745d4ea35d63ac76c97ef82c90bc
jinheel1 <jinheel1@andrew.cmu.edu>
2017-12-10 18:58:56 -0500

    Create hello_aliens.R

---
+++ hello_aliens.R
@@ -0,0 +1,8 @@
+cat("Hello, aliens. Rather than ask questions like where you come from or why you're here, 
+I wanna ask which of our fictional works managed to portray your particular race the closest.
+Considering the fact we're not dying horribly by your doing right now, I'd guess you're not of
+Ridley Scott's Aliens or DC's Brainiac and what have you. Mass Effect or Star Wars would be pretty
+cool, since that'd mean humans could eventually integrate with the intergalactic happenings. Saiyans
+would be rough, but then millions of little kids could finally live out their dreams of seeing
+Super Saiyans. Whatever the case, I'm excited to hear your thoughts. As a side note, I'm amazed that
+my planet chose me to talk to you. Toodles!")
